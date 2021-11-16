be3ea2bbb2d13afa7e1aad09fef52045ddbe17e0
Your Name <you@example.com>
2021-11-16 20:17:45 +0100

    Initial commit

---
+++ src/calc.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class calc {
+    Scanner
+    public static void main(String[] args) {
+
+
+        static int add(int x, int y)
+        static int subtract(int x, int y)
+        static int multiply(int x, int y)
+        static int divide(int x, int y)
+        static int remainder(int x, int y)
+    }
+}
---
+++ testesitos/tes.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class tes {
+    @Test
+    public void calculadora() {
+        Assert.assertTrue(true);
+    }
+}
+xº
