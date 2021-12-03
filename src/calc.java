a0f6c8e930ca1887835a5f1d82a743ccac325df7
Your Name <you@example.com>
2021-12-03 16:21:07 +0100

    divide exclusive

--- src/calc.java
+++ src/calc.java
@@ -1,29 +1,48 @@
 import java.util.Scanner;
 public class calc {
 
-   /* public static int operate(int x, int y) {
-        int resultat = 0;
-         static int add(int x, int y) {
-            resultat = x+y;
-        }
-         static int subtract(int x, int y) {
-             resultat = x-y;
-        }
-         static int multiply(int x, int y) {
-             resultat = x*y;
-        }
-         static int divide(int x, int y) {
-             resultat = x/y;
-        }
-         static int remainder(int x, int y) {
-             resultat =
-        }
-         static double add(double x, double y) {}
-         static double subtract(double x, double y) {}
-         static double multiply(double x, double y) {}
-         static double divide(double x, double y) {}
-         static double power(double b, double e) {}
-
+    /* static int add(int x, int y) {
+         int resultat = x + y;
         return resultat;
-    }*/
-}
+    }
+     static int subtract(int x, int y) {
+         int resultat = x-y;
+         return resultat;
+    }
+     static int multiply(int x, int y) {
+         int resultat = x*y;
+         return resultat;
+
+     }
+     static int divide(int x, int y) {
+         int resultat = x/y;
+         return resultat;
+
+     }
+     static int remainder(int x, int y) {
+         int resultat = x%y;
+         return resultat;
+     }
+     static double add(double x, double y) {
+         double resultat = x + y;
+         return resultat;
+     }
+     static double subtract(double x, double y) {
+         double resultat = x - y;
+         return resultat;
+     }
+     static double multiply(double x, double y) {
+         double resultat = x * y;
+         return resultat;
+     } */
+         static double divide(double x, double y) {
+             double resultat = x / y;
+             return resultat;
+         }
+      /*   static double power(double b, double e) {
+             double resultat = x%y;
+             return resultat;
+         }
+*/
+    }
+
--- testesitos/tes.java
+++ testesitos/tes.java
@@ -1,10 +1,17 @@
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class tes {
     @Test
     public void test1() {
-        //assertEquals("One", calc.operate(1));
 
+        assertEquals(1.5, calc.divide(3.0, 2.0), 0);
+
+        assertEquals(3.5, calc.divide(7.0, 2.0), 0);
+
+        assertEquals(15.0, calc.divide(30.0, 2.0), 0);
+
+        assertEquals(3.5, calc.divide(70.0, 20.0), 0);
     }
 }
