37d2c5fbd0ea99e140133e0c5ad8f16ab64b9654
Your Name <you@example.com>
2021-12-04 16:59:39 +0100

    AllComplet

--- src/calc.java
+++ src/calc.java
@@ -1,48 +1,58 @@
 import java.util.Scanner;
+
 public class calc {
 
     static int add(int x, int y) {
-         int resultat = x + y;
+        int resultat = x + y;
         return resultat;
     }
-     static int subtract(int x, int y) {
-         int resultat = x-y;
-         return resultat;
-    }
-     static int multiply(int x, int y) {
-         int resultat = x*y;
-         return resultat;
 
-     }
-     static int divide(int x, int y) {
-         int resultat = x/y;
-         return resultat;
-
-     }
-     static int remainder(int x, int y) {
-         int resultat = x%y;
-         return resultat;
-     }
-     static double add(double x, double y) {
-         double resultat = x + y;
-         return resultat;
-     }
-     static double subtract(double x, double y) {
-         double resultat = x - y;
-         return resultat;
-     }
-     static double multiply(double x, double y) {
-         double resultat = x * y;
-         return resultat;
-     }
-         static double divide(double x, double y) {
-             double resultat = x / y;
-             return resultat;
-         }
-      /*   static double power(double b, double e) {
-             double resultat = x%y;
-             return resultat;
-         }
-*/
+    static int subtract(int x, int y) {
+        int resultat = x - y;
+        return resultat;
     }
+
+    static int multiply(int x, int y) {
+        int resultat = x * y;
+        return resultat;
+
+    }
+
+    static int divide(int x, int y) {
+        int resultat = x / y;
+        return resultat;
+
+    }
+
+    static int remainder(int x, int y) {
+        int resultat = x % y;
+        return resultat;
+    }
+
+    static double add(double x, double y) {
+        double resultat = x + y;
+        return resultat;
+    }
+
+    static double subtract(double x, double y) {
+        double resultat = x - y;
+        return resultat;
+    }
+
+    static double multiply(double x, double y) {
+        double resultat = x * y;
+        return resultat;
+    }
+
+    static double divide(double x, double y) {
+        double resultat = x / y;
+        return resultat;
+    }
+
+    static double power(double x, double y) {
+        double resultat = x % y;
+        return resultat;
+    }
+
+}
 
--- testesitos/tes.java
+++ testesitos/tes.java
@@ -4,23 +4,38 @@ import static org.junit.Assert.assertEquals;
 
 public class tes {
     @Test
-    public void test1() {
+    public void testInt() {
         assertEquals(1, calc.add(0, 1));
+        assertEquals(5, calc.add(4, 1));
+        assertEquals(44, calc.add(34, 10));
 
         assertEquals(1, calc.subtract(2, 1));
+        assertEquals(2, calc.subtract(3, 1));
+        assertEquals(19, calc.subtract(20, 1));
 
         assertEquals(1, calc.multiply(1, 1));
+        assertEquals(25, calc.multiply(5, 5));
+        assertEquals(0, calc.multiply(100, 0));
 
         assertEquals(1, calc.divide(2, 2));
+        assertEquals(1, calc.divide(10, 10));
+        assertEquals(4, calc.divide(8, 2));
 
         assertEquals(0, calc.remainder(2, 2));
+        assertEquals(1, calc.remainder(5, 2));
+        assertEquals(0, calc.remainder(47, 1));
 
     }
-    public void test2() {
+    @Test
+    public void testDouble() {
 
         assertEquals(1.5, calc.add(0, 1.5), 0);
+        assertEquals(2.5, calc.add(1.0, 1.5), 0);
+        assertEquals(3.0, calc.add(1.5, 1.5), 0);
 
-        assertEquals(0.5, calc.subtract(2, 1.5), 0);
+        assertEquals(0.5, calc.subtract(2.0, 1.5), 0);
+        assertEquals(1.0, calc.subtract(2.5, 1.5), 0);
+        assertEquals(18.5, calc.subtract(20.0, 1.5), 0);
 
         assertEquals(1.5, calc.multiply(1, 1.5), 0);
         assertEquals(2.5, calc.multiply(1, 2.5), 0);
@@ -30,5 +45,8 @@ public class tes {
         assertEquals(15.0, calc.divide(30.0, 2.0), 0);
         assertEquals(3.5, calc.divide(70.0, 20.0), 0);
 
+        assertEquals(0.0, calc.power(2.5, 2.5), 0);
+        assertEquals(2.0, calc.power(50.0, 3.0), 0);
+        assertEquals(0.0, calc.power(47, 1), 0);
     }
 }
